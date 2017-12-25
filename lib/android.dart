f53b82df97858b76178431c90066345694fd0374
Mark O'Sullivan <mos182@hotmail.co.uk>
2017-12-25 20:08:18 +0000

    Can now successfully switch Android launcher icon to new icon without overwriting the old one

--- lib/android.dart
+++ lib/android.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 const String android_res_folder = "android/app/src/main/res/";
 const String android_manifest_file = "android/app/src/main/AndroidManifest.xml";
 const String android_file_name ="ic_launcher.png";
+const String default_icon_name = "ic_launcher";
 
 //
 class AndroidIcons {
@@ -39,12 +40,15 @@ convertAndroid(config) {
     String file_path = config['flutter_icons']['image_path'];
     Image image = decodeImage(new File(file_path).readAsBytesSync());
     if (config['flutter_icons']['overwrite']) {
-      print("Overwriting default Android flutter launcher icon");
-      //android_icons.forEach((AndroidIcons e) => replaceDefaultIcon(e, image));
+      print("Saving new icon to ic_launcher.png and switching Android launcher icon to it");
+      android_icons.forEach((AndroidIcons e) => replaceDefaultIcons(e, image));
+      changeAndroidLauncherIcon(default_icon_name);
     } else {
+      String icon_name = config['flutter_icons']['icon_name'];
+      String icon_path = icon_name + ".png";
       print("Creating new Android launcher icon");
-      android_icons.forEach((AndroidIcons e) => saveIcons(e, image, "test.png"));
-      changeAndroidLauncherIcon();
+      android_icons.forEach((AndroidIcons e) => saveIcons(e, image, icon_path));
+      changeAndroidLauncherIcon(icon_name);
     }
     print("Android Launcher Images Generated Successfully");
 }
@@ -56,13 +60,13 @@ replaceDefaultIcons(AndroidIcons e, image) {
 }
 
 saveIcons(AndroidIcons e, image, String iconFilePath) {
-  //Image newFile = copyResize(image, e.size);
-  //new Io.File(android_res_folder + e.name + '/' + iconFilePath)
-  //..writeAsBytesSync(encodePng(newFile));
-
+  Image newFile = copyResize(image, e.size);
+  new File(android_res_folder + e.name + '/' + iconFilePath)
+  ..writeAsBytesSync(encodePng(newFile));
 }
+
 // NOTE: default = ic_launcher
-changeAndroidLauncherIcon() async {
+changeAndroidLauncherIcon(String icon_name) async {
   File androidManifestFile = new File(android_manifest_file);
   List<String> lines = await androidManifestFile.readAsLines();
   for (var x = 0; x < lines.length; x++) {
@@ -71,20 +75,12 @@ changeAndroidLauncherIcon() async {
       print("OLD LINE");
       print(line);
       // Using RegExp replace the value of android:icon to point to the new icon
-      line = line.replaceAll(new RegExp('android:icon=\"([^*]|(\"+([^"/]|)))*\"'), 'android:icon="@mipmap/ic_test2"');
+      line = line.replaceAll(new RegExp('android:icon=\"([^*]|(\"+([^"/]|)))*\"'), 'android:icon="@mipmap/' + icon_name + '"');
       print("NEW LINE");
       print(line);
       lines[x] = line;
     }
   }
-  lines.forEach((line) {
-    if (line.contains("android:icon")) {
-      print("FOR EACH LINE");
-      print(line);
-    }
-  });
   androidManifestFile.writeAsString(lines.join("\n"));
-  //print("PRINTING ENTIRE FILE");
-  //print(lines);
 }
 
