937122526d202b4983e9f5daf05c30405c5e21dd
Mark O'Sullivan <mos182@hotmail.co.uk>
2018-05-30 20:36:37 +0100

    Removed the old refactored code and replaced it with unit tested code

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,7 @@ import 'package:flutter_launcher_icons/android.dart' as AndroidLauncherIcons;
 import 'package:flutter_launcher_icons/ios.dart' as IOSLauncherIcons;
 import 'package:dart_config/default_server.dart';
 
-createIcons(List<String> arguments) {
-  Future<Map> conf = loadConfig("pubspec.yaml");
-  conf.then((Map config) {
-    if (config['flutter_icons']['image_path'] != null) {
-      var androidConfig = config['flutter_icons']['android'];
-      var iosConfig = config['flutter_icons']['ios'];
-      if (androidConfig == true || androidConfig is String) {
-        AndroidLauncherIcons.createIcons(config);
-      }
-      if (iosConfig == true || iosConfig is String) {
-        IOSLauncherIcons.createIcons(config);
-      }
-      if (((androidConfig == false) && (iosConfig == false)) ||
-          ((androidConfig == null) && (iosConfig == null))) {
-        print(
-            "Error: No platform has been specified to generate launcher icons for.");
-      } else {
-        print("Finished!");
-      }
-    } else {
-      print("flutter_icons config not found in pubspec.yaml");
-    }
-  });
-}
-
-createIcons2(List<String> arguments) async {
+createIcons(List<String> arguments) async {
   loadConfigFile("pubspec.yaml").then((Map yamlConfig) {
     Map config = loadFlutterIconsConfig(yamlConfig);
     if (!isImagePathInConfig(config)) {
