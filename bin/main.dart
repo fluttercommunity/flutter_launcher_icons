1df310620c9ced14a043856c2d0aa30da4dc8467
Alexandre Roux <alex@tekartik.com>
2018-11-16 23:55:45 +0100

    feat: try flutter_launcher_icons.yaml config file first and allow overriding configFile (#60)
    * feat: try flutter_launcher_icons.yaml config file first and allow overriding config file
    
    * feat: more tests on config file from arg result
    
    * doc: help usage
    
    * feat: add custom exception NoConfigFoundException and display it when the config file is not found of if there is no flutter_icons section

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_launcher_icons/main.dart' as FlutterLauncherIcons;
 
 main(List<String> arguments) {
-  FlutterLauncherIcons.createIcons(arguments);
+  FlutterLauncherIcons.createIconsFromArguments(arguments);
 }
