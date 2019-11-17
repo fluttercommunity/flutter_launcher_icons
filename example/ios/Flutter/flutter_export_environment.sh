fd4142215c866b23f84dec58baccc93c6e7182f4
Mark O'Sullivan <mark@argent.im>
2019-11-17 23:51:50 +0000

    running into issues with inkscape arguments

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,8 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mos/Flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/mos/FlutterProjects/flutter_launcher_icons/example"
-export "FLUTTER_TARGET=/Users/mos/FlutterProjects/flutter_launcher_icons/example/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/mos/Flutter/bin/cache/artifacts/engine/ios"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
