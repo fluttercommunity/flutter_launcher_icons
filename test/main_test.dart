7ad838107c8f8162a29431e6f0a5888b322af203
Henry Heino <personalizedrefrigerator@gmail.com>
2020-11-06 20:53:58 -0800

    Add test to ensure version consistency between README, CHANGELOG, and contants.dart

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,14 +1,12 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
-import 'package:path/path.dart';
 import 'package:test/test.dart';
+import 'package:path/path.dart' as pathlib;
 import 'package:flutter_launcher_icons/ios.dart' as ios;
 import 'package:flutter_launcher_icons/android.dart' as android;
 import 'package:flutter_launcher_icons/main.dart' as main_dart;
 import 'package:flutter_launcher_icons/constants.dart' as constants;
-
-
 
 // Unit tests for main.dart
 void main() {
@@ -34,24 +32,26 @@ void main() {
 
   group('config file from args', () {
     // Create mini parser with only the wanted option, mocking the real one
-    final ArgParser parser = ArgParser()..addOption(constants.fileOption, abbr: 'f');
-    final String testDir =
-        join('.dart_tool', 'flutter_launcher_icons', 'test', 'config_file');
+    final ArgParser parser = ArgParser()
+      ..addOption(constants.fileOption, abbr: 'f');
+    final String testDir = pathlib.join(
+        '.dart_tool', 'flutter_launcher_icons', 'test', 'config_file');
 
     String currentDirectory = testDir;
     Future<void> setCurrentDirectory(String path) async {
-      currentDirectory = join(testDir, path);
+      currentDirectory = pathlib.join(testDir, path);
       await Directory(currentDirectory).create(recursive: true);
     }
 
     File getFile(String filename) {
-      final String path = join(currentDirectory, filename);
+      final String path = pathlib.join(currentDirectory, filename);
 
       return File(path);
     }
 
     Map<String, dynamic> loadConfig(ArgResults args) {
-      return main_dart.loadConfigFileFromArgResults(args, cwd: currentDirectory);
+      return main_dart.loadConfigFileFromArgResults(args,
+          cwd: currentDirectory);
     }
 
     test('default', () async {
@@ -106,10 +106,13 @@ flutter_icons:
 
   test('Incorrect pubspec.yaml path throws correct error message', () async {
     const String incorrectPath = 'test/config/test_pubspec.yam';
-    expect(() => main_dart.loadConfigFile(incorrectPath, null), throwsA(const TypeMatcher<FileSystemException>()));
+    expect(() => main_dart.loadConfigFile(incorrectPath, null),
+        throwsA(const TypeMatcher<FileSystemException>()));
   });
 
-  test('image_path is in config -- All platforms set to true & image_path given.', () {
+  test(
+      'image_path is in config -- All platforms set to true & image_path given.',
+      () {
     final Map<String, dynamic> flutterIconsConfig = <String, dynamic>{
       'image_path': 'assets/images/icon-710x599.png',
       'android': true,
@@ -119,8 +122,9 @@ flutter_icons:
     expect(main_dart.isConfigValid(flutterIconsConfig), true);
   });
 
-  test('image_path is in config -- android & ios requested, but only android given',
-  () {
+  test(
+      'image_path is in config -- android & ios requested, but only android given',
+      () {
     final Map<String, dynamic> flutterIconsConfigAndroid = <String, dynamic>{
       'image_path_android': 'assets/images/icon-710x599.png',
       'android': true,
@@ -129,8 +133,9 @@ flutter_icons:
     expect(main_dart.isConfigValid(flutterIconsConfigAndroid), false);
   });
 
-  test('image_path is in config -- Android and iOS both active and icons given for both',
-  () {
+  test(
+      'image_path is in config -- Android and iOS both active and icons given for both',
+      () {
     final Map<String, dynamic> flutterIconsConfigIOSAndroid = <String, dynamic>{
       'image_path_android': 'assets/images/icon-710x599.png',
       'image_path_ios': 'assets/images/icon-710x599.png',
@@ -140,8 +145,7 @@ flutter_icons:
     expect(main_dart.isConfigValid(flutterIconsConfigIOSAndroid), true);
   });
 
-  test('image_path is in config -- Icons for all systems given.',
-  () {
+  test('image_path is in config -- Icons for all systems given.', () {
     final Map<String, dynamic> flutterIconsConfigAll = <String, dynamic>{
       'image_path_android': 'assets/images/icon-710x599.png',
       'image_path_ios': 'assets/images/icon-710x599.png',
@@ -153,8 +157,9 @@ flutter_icons:
     expect(main_dart.isConfigValid(flutterIconsConfigAll), true);
   });
 
-  test('image_path is in config -- only image_path for ios given & does not match target (web).',
-  () {
+  test(
+      'image_path is in config -- only image_path for ios given & does not match target (web).',
+      () {
     final Map<String, dynamic> flutterIconsConfigWeb = <String, dynamic>{
       'image_path_ios': 'assets/images/icon-710x599.png',
       'web': true,
@@ -221,7 +226,7 @@ flutter_icons:
     };
     expect(main_dart.platforms['ios'].inConfig(flutterIconsConfig), true);
   });
-  
+
   test('No new iOS icon needed -- only Android and web configs', () {
     final Map<String, dynamic> flutterIconsConfig = <String, dynamic>{
       'image_path': 'assets/images/icon-710x599.png',
@@ -230,7 +235,7 @@ flutter_icons:
     };
     expect(main_dart.platforms['ios'].inConfig(flutterIconsConfig), false);
   });
-  
+
   test('No new web icon needed -- web: false', () {
     final Map<String, dynamic> flutterIconsConfig = <String, dynamic>{
       'image_path': 'assets/images/icon-710x599.png',
@@ -238,5 +243,56 @@ flutter_icons:
       'ios': true
     };
     expect(main_dart.platforms['web'].inConfig(flutterIconsConfig), false);
+  });
+
+  final RegExp versionCodeExp =
+      RegExp(r'\s?[#]{2,4}\s+(?:Version\s+)?(\d+\.\d+\.\d+)\s+\(.*\)\s*');
+
+  test('Version code regexp works.', () {
+    expect(versionCodeExp.firstMatch('#### 0.0.1 (3rd Oct 2020)'), isNotNull);
+    expect(versionCodeExp.firstMatch('Not a version code.'), isNull);
+    expect(versionCodeExp.firstMatch('## Not a version code'), isNull);
+    expect(versionCodeExp.firstMatch('## 4.4 is not a .0 version'), isNull);
+    expect(
+        versionCodeExp.firstMatch('## 102.23.44 (3rd Never 2020)'), isNotNull);
+    expect(versionCodeExp.firstMatch('\n## 102.23.44 (3rd Never 2020)'),
+        isNotNull);
+    expect(versionCodeExp.firstMatch('## 102.23.44 (3rd Never 2020)\n'),
+        isNotNull);
+    expect(
+        versionCodeExp
+            .firstMatch('\n\n456\n## 102.23.44 (3rd Never 2020)\n123'),
+        isNotNull);
+  });
+
+  test('Built-in version code matches that in CHANGELOG.md', () async {
+    final File changelog = File('CHANGELOG.md');
+    final String changelogContent = await changelog.readAsString();
+
+    // Search for & extract the first version code in the changelog.
+
+    final RegExpMatch firstVersionMatch =
+        versionCodeExp.firstMatch(changelogContent);
+
+    expect(firstVersionMatch, isNotNull);
+
+    // firstVersion is the first found in the file, (not the oldest version).
+    final String firstVersion = firstVersionMatch.group(1);
+    expect(firstVersion, equals(constants.currentVersion));
+  });
+
+  test('Built-in version code matches that in README.md', () async {
+    final File readme = File('README.md');
+    final String readmeContent = await readme.readAsString();
+
+    final RegExpMatch firstVersionMatch =
+        versionCodeExp.firstMatch(readmeContent);
+
+    // Eventually, versions might not be present in README.md...
+    if (firstVersionMatch != null) {
+      final String readmeVersion = firstVersionMatch.group(1);
+
+      expect(readmeVersion, equals(constants.currentVersion));
+    }
   });
 }
