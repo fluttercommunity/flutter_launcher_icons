24bc6e7f60886c91e9a3d3e60e605300e47ee79e
Ratakondala Arun <ratakondalaarun@gmail.com>
2022-07-10 20:38:01 +0530

    test: added common entrypoint to all tests
    This can be used to generate code coverage reports

---
+++ test/all_tests.dart
@@ -0,0 +1,25 @@
+import 'package:test/test.dart';
+
+import 'abs/icon_generator_test.dart' as icon_generator_test;
+import 'android_test.dart' as android_test;
+import 'flutter_launcher_icons_config_test.dart' as fli_config;
+import 'main_test.dart' as main_test;
+import 'utils_test.dart' as utils_test;
+import 'web/web_template_test.dart' as web_template_test;
+import 'web/web_icon_generator_test.dart' as web_icon_gen_test;
+
+void main() {
+  group('Flutter launcher icons', () {
+    // others
+    utils_test.main();
+    fli_config.main();
+    icon_generator_test.main();
+
+    main_test.main();
+    // android
+    android_test.main();
+    // web
+    web_template_test.main();
+    web_icon_gen_test.main();
+  });
+}
