d3c061d572cfff14cb2d18462b04bc98d097ae84
Ratakondala Arun <ratakondalaarun@gmail.com>
2022-07-10 12:54:04 +0530

    test(fli_config): added tests for fli config

---
+++ test/flutter_launcher_icons_config_test.dart
@@ -0,0 +1,152 @@
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/flutter_launcher_icons_config.dart';
+import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
+import 'package:test_descriptor/test_descriptor.dart' as d;
+
+import './templates.dart' as templates;
+
+void main() {
+  group('FlutterLauncherIconsConfig', () {
+    late String prefixPath;
+    setUpAll(() {
+      prefixPath = path.join(d.sandbox, 'fli_test');
+    });
+    group('#loadConfigFromPath', () {
+      setUpAll(() async {
+        await d.dir('fli_test', [
+          d.file('flutter_launcher_icons.yaml', templates.fliConfigTemplate),
+          d.file('invalid_fli_config.yaml', templates.invlaidfliConfigTemplate),
+        ]).create();
+      });
+      test('should return valid configs', () {
+        final configs = FlutterLauncherIconsConfig.loadConfigFromPath('flutter_launcher_icons.yaml', prefixPath);
+        expect(configs, isNotNull);
+        // android configs
+        expect(configs!.android, isTrue);
+        expect(configs.imagePath, isNotNull);
+        expect(configs.imagePathAndroid, isNotNull);
+        expect(configs.adaptiveIconBackground, isNotNull);
+        expect(configs.adaptiveIconForeground, isNotNull);
+        // ios configs
+        expect(configs.ios, isTrue);
+        expect(configs.imagePathIOS, isNotNull);
+        // web configs
+        expect(configs.webConfig, isNotNull);
+        expect(configs.webConfig!.generate, isTrue);
+        expect(configs.webConfig!.backgroundColor, isNotNull);
+        expect(configs.webConfig!.imagePath, isNotNull);
+        expect(configs.webConfig!.themeColor, isNotNull);
+        expect(
+          configs.webConfig!.toJson(),
+          equals(<String, dynamic>{
+            'generate': true,
+            'image_path': 'app_icon.png',
+            'background_color': '#0175C2',
+            'theme_color': '#0175C2',
+          }),
+        );
+      });
+
+      test('should return null when invalid filePath is given', () {
+        final configs = FlutterLauncherIconsConfig.loadConfigFromPath('file_that_dont_exist.yaml', prefixPath);
+        expect(configs, isNull);
+      });
+
+      test('should throw InvalidConfigException when config is invalid', () {
+        expect(
+          () => FlutterLauncherIconsConfig.loadConfigFromPath('invalid_fli_config.yaml', prefixPath),
+          throwsA(isA<InvalidConfigException>()),
+        );
+      });
+    });
+
+    group('#loadConfigFromPubSpec', () {
+      setUpAll(() async {
+        await d.dir('fli_test', [
+          d.file('pubspec.yaml', templates.pubspecTemplate),
+          d.file('flutter_launcher_icons.yaml', templates.fliConfigTemplate),
+          d.file('invalid_fli_config.yaml', templates.invlaidfliConfigTemplate),
+        ]).create();
+      });
+      test('should return valid configs', () {
+        final configs = FlutterLauncherIconsConfig.loadConfigFromPubSpec(prefixPath);
+        expect(configs, isNotNull);
+        // android configs
+        expect(configs!.android, isTrue);
+        expect(configs.imagePath, isNotNull);
+        expect(configs.imagePathAndroid, isNotNull);
+        expect(configs.adaptiveIconBackground, isNotNull);
+        expect(configs.adaptiveIconForeground, isNotNull);
+        // ios configs
+        expect(configs.ios, isTrue);
+        expect(configs.imagePathIOS, isNotNull);
+        // web configs
+        expect(configs.webConfig, isNotNull);
+        expect(configs.webConfig!.generate, isTrue);
+        expect(configs.webConfig!.backgroundColor, isNotNull);
+        expect(configs.webConfig!.imagePath, isNotNull);
+        expect(configs.webConfig!.themeColor, isNotNull);
+        expect(
+          configs.webConfig!.toJson(),
+          equals(<String, dynamic>{
+            'generate': true,
+            'image_path': 'app_icon.png',
+            'background_color': '#0175C2',
+            'theme_color': '#0175C2',
+          }),
+        );
+      });
+
+      group('should throw', () {
+        setUp(() async {
+          await d.dir('fli_test', [
+            d.file('pubspec.yaml', templates.invalidPubspecTemplate),
+            d.file('flutter_launcher_icons.yaml', templates.fliConfigTemplate),
+            d.file('invalid_fli_config.yaml', templates.invlaidfliConfigTemplate),
+          ]).create();
+        });
+        test('InvalidConfigException when config is invalid', () {
+          expect(
+            () => FlutterLauncherIconsConfig.loadConfigFromPubSpec(prefixPath),
+            throwsA(isA<InvalidConfigException>()),
+          );
+        });
+      });
+    });
+    group('#loadConfigFromFlavor', () {
+      setUpAll(() async {
+        await d.dir('fli_test', [
+          d.file('flutter_launcher_icons-development.yaml', templates.flavorFLIConfigTemplate),
+        ]).create();
+      });
+      test('should return valid config', () {
+        final configs = FlutterLauncherIconsConfig.loadConfigFromFlavor('development', prefixPath);
+        expect(configs, isNotNull);
+        expect(configs!.android, isTrue);
+        expect(configs.imagePath, isNotNull);
+        expect(configs.imagePathAndroid, isNotNull);
+        expect(configs.adaptiveIconBackground, isNotNull);
+        expect(configs.adaptiveIconForeground, isNotNull);
+        // ios configs
+        expect(configs.ios, isTrue);
+        expect(configs.imagePathIOS, isNotNull);
+        // web configs
+        expect(configs.webConfig, isNotNull);
+        expect(configs.webConfig!.generate, isTrue);
+        expect(configs.webConfig!.backgroundColor, isNotNull);
+        expect(configs.webConfig!.imagePath, isNotNull);
+        expect(configs.webConfig!.themeColor, isNotNull);
+        expect(
+          configs.webConfig!.toJson(),
+          equals(<String, dynamic>{
+            'generate': true,
+            'image_path': 'app_icon.png',
+            'background_color': '#0175C2',
+            'theme_color': '#0175C2',
+          }),
+        );
+      });
+    });
+  });
+}
---
+++ test/templates.dart
@@ -0,0 +1,249 @@
+const fliConfigTemplate = r'''
+flutter_icons:
+  android: true
+  ios: true
+  image_path: "assets/images/icon-128x128.png"
+  image_path_android: "assets/images/icon-710x599-android.png"
+  image_path_ios: "assets/images/icon-1024x1024.png"
+  adaptive_icon_background: "assets/images/christmas-background.png"
+  adaptive_icon_foreground: "assets/images/icon-foreground-432x432.png"
+  web:
+    generate: true
+    image_path: "app_icon.png" # filepath
+    background_color: "#0175C2" # hex_color
+    theme_color: "#0175C2" # hex_color
+    apple_mobile_web_app_title: "demo"
+    apple_mobile_web_app_status_bar_style: "hex_color"
+''';
+
+const flavorFLIConfigTemplate = fliConfigTemplate;
+
+const invlaidfliConfigTemplate = r'''
+# flutter_icons
+android: true
+ios: true
+image_path: "assets/images/icon-128x128.png"
+ ad
+image_path_android: "assets/images/icon-710x599-android.png"
+image_path_ios: "assets/images/icon-1024x1024.png"
+adaptive_icon_background: "assets/images/christmas-background.png"
+adaptive_icon_foreground: "assets/images/icon-foreground-432x432.png"
+web:
+  generate: true
+  image_path: "app_icon.png" # filepath
+  background_color: "#0175C2" # hex_color
+  theme_color: "#0175C2" # hex_color
+  apple_mobile_web_app_title: "demo"
+  apple_mobile_web_app_status_bar_style: "hex_color"
+''';
+
+const pubspecTemplate = r'''
+name: demo
+description: A new Flutter project.
+publish_to: 'none'
+version: 1.0.0+1
+
+environment:
+  sdk: '>=2.18.0-44.1.beta <3.0.0'
+
+dependencies:
+  flutter:
+    sdk: flutter
+  cupertino_icons: ^1.0.2
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^2.0.0
+  flutter_launcher_icons:
+    path: C:/Users/asus/projects/flutter_launcher_icons
+
+flutter:
+  uses-material-design: true
+  assets:
+    - images/a_dot_burr.jpeg
+    - images/a_dot_ham.jpeg
+  fonts:
+    - family: Schyler
+      fonts:
+        - asset: fonts/Schyler-Regular.ttf
+        - asset: fonts/Schyler-Italic.ttf
+          style: italic
+    - family: Trajan Pro
+      fonts:
+        - asset: fonts/TrajanPro.ttf
+        - asset: fonts/TrajanPro_Bold.ttf
+          weight: 700
+
+flutter_icons:
+  android: true
+  ios: true
+  image_path: "assets/images/icon-128x128.png"
+  image_path_android: "assets/images/icon-710x599-android.png"
+  image_path_ios: "assets/images/icon-1024x1024.png"
+  adaptive_icon_background: "assets/images/christmas-background.png"
+  adaptive_icon_foreground: "assets/images/icon-foreground-432x432.png"
+  web:
+    generate: true
+    image_path: "app_icon.png" # filepath
+    background_color: "#0175C2" # hex_color
+    theme_color: "#0175C2" # hex_color
+    apple_mobile_web_app_title: "demo"
+    apple_mobile_web_app_status_bar_style: "hex_color"
+''';
+
+const invalidPubspecTemplate = r'''
+name: demo
+description: A new Flutter project.
+publish_to: 'none'
+version: 1.0.0+1
+
+environment:
+  sdk: '>=2.18.0-44.1.beta <3.0.0'
+
+dependencies:
+  flutter:
+    sdk: flutter
+  cupertino_icons: ^1.0.2
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^2.0.0
+  flutter_launcher_icons:
+    path: C:/Users/asus/projects/flutter_launcher_icons
+
+flutter:
+  uses-material-design: true
+  assets:
+    - images/a_dot_burr.jpeg
+    - images/a_dot_ham.jpeg
+  fonts:
+    - family: Schyler
+      fonts:
+        - asset: fonts/Schyler-Regular.ttf
+        - asset: fonts/Schyler-Italic.ttf
+          style: italic
+    - family: Trajan Pro
+      fonts:
+        - asset: fonts/TrajanPro.ttf
+        - asset: fonts/TrajanPro_Bold.ttf
+          weight: 700
+
+flutter_icons:
+  android: true
+ invalid_indented_key_key
+  ios: true
+  image_path: "assets/images/icon-128x128.png"
+  image_path_android: "assets/images/icon-710x599-android.png"
+  image_path_ios: "assets/images/icon-1024x1024.png"
+  adaptive_icon_background: "assets/images/christmas-background.png"
+  adaptive_icon_foreground: "assets/images/icon-foreground-432x432.png"
+  web:
+    generate: true
+    image_path: "app_icon.png" # filepath
+    background_color: "#0175C2" # hex_color
+    theme_color: "#0175C2" # hex_color
+    apple_mobile_web_app_title: "demo"
+    apple_mobile_web_app_status_bar_style: "hex_color"
+''';
+
+const webManifestTemplate = r'''
+{
+  "name": "demo",
+  "short_name": "demo",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#0175C2",
+  "theme_color": "#0175C2",
+  "description": "A new Flutter project.",
+  "orientation": "portrait-primary",
+  "prefer_related_applications": false,
+  "icons": [
+    {
+      "src": "icons/Icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/Icon-maskable-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "maskable"
+    },
+    {
+      "src": "icons/Icon-maskable-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ]
+}
+''';
+
+const webIndexTemplate = r'''
+<!DOCTYPE html>
+<html>
+<head>
+  <!--
+    If you are serving your web app in a path other than the root, change the
+    href value below to reflect the base path you are serving from.
+
+    The path provided below has to start and end with a slash "/" in order for
+    it to work correctly.
+
+    For more details:
+    * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/base
+
+    This is a placeholder for base href that will be replaced by the value of
+    the `--base-href` argument provided to `flutter build`.
+  -->
+  <base href="$FLUTTER_BASE_HREF">
+
+  <meta charset="UTF-8">
+  <meta content="IE=Edge" http-equiv="X-UA-Compatible">
+  <meta name="description" content="A new Flutter project.">
+
+  <!-- iOS meta tags & icons -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="demo">
+  <link rel="apple-touch-icon" href="icons/Icon-192.png">
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="favicon.png"/>
+
+  <title>demo</title>
+  <link rel="manifest" href="manifest.json">
+
+  <script>
+    // The value below is injected by flutter build, do not touch.
+    var serviceWorkerVersion = null;
+  </script>
+  <!-- This script adds the flutter initialization JS code -->
+  <script src="flutter.js" defer></script>
+</head>
+<body>
+  <script>
+    window.addEventListener('load', function(ev) {
+      // Download main.dart.js
+      _flutter.loader.loadEntrypoint({
+        serviceWorker: {
+          serviceWorkerVersion: serviceWorkerVersion,
+        }
+      }).then(function(engineInitializer) {
+        return engineInitializer.initializeEngine();
+      }).then(function(appRunner) {
+        return appRunner.runApp();
+      });
+    });
+  </script>
+</body>
+</html>
+
+''';
