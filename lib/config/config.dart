72f30aee03d162ac0d3aeab414a10cfea8ec8c5b
OutdatedGuy <everythingoutdated@gmail.com>
2023-12-30 00:32:47 +0530

    fix: incorrect doc for adaptiveIconMonochrome config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -117,15 +117,15 @@ class Config {
   @JsonKey(name: 'image_path_ios')
   final String? imagePathIOS;
 
-  /// android adaptive_icon_foreground image
+  /// Android adaptive_icon_foreground image path
   @JsonKey(name: 'adaptive_icon_foreground')
   final String? adaptiveIconForeground;
 
-  /// android adaptive_icon_background image
+  /// Android adaptive_icon_background image path
   @JsonKey(name: 'adaptive_icon_background')
   final String? adaptiveIconBackground;
 
-  /// android adaptive_icon_background image
+  /// Android adaptive_icon_monochrome image path
   @JsonKey(name: 'adaptive_icon_monochrome')
   final String? adaptiveIconMonochrome;
 
